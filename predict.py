2104374ef57664b98c336cbe6a6a76e278b77d01
unknown <ooa_747@hotmail.com>
2021-02-05 05:03:07 +0300

    secand commmie

--- predict.py
+++ predict.py
@@ -1,6 +1,4 @@
-!pip install tensorflow_datasets as tfds
-!pip install tensorflow 
-!pip install -q -U  tensorflow-hub
+
 import warnings
 warnings.filterwarnings('ignore')
 import numpy as np
@@ -18,25 +16,38 @@ from tensorflow import keras
 from tensorflow.keras import layers
 from PIL import Image
 from tensorflow.keras.preprocessing.image 
+from module import sub-module
+
+
 %matplotlib inline
 %config InlineBackend.figure_format = 'retina'
  
 parser = argparse.ArgumentParser()
+ 
+
 
 parser.add_argument('-checkpoint',help='Checkpoint of the model')
 parser.add_argument('--gpu', action='store_true',  default=False,dest='gpu', help='Set training to gpu')
-parser.add_argument('--top_k', action='store', dest='topk', default=5, help='Top k prediction probabilities')
+parser.add_argument('--top_k', default=5, dest="top_k", action="store", type=int)
 parser.add_argument('-image_dir', default='./test_image/hard-leaved_pocket_orchid.jpg',help='Path to image,',type=str)
 parser.add_argument('--category_names', action='store',dest='category_names', default=None,help='Json association between category and names')
+parser.add_argument('image_path',help='Image directory path')
+parser.add_argument('image_path', help='image  path')
 
-top_k = commands.top_k
+
+ 
+
+commands = parser.parse_args()
+#top_k = commands.top_k
+top_k = commands.topk
 image_path = commands.checkpoint
 classes = commands.category_names
 commands , unknown = parser.parse_known_args()
 
-
-reload_keras_model = tf.keras.models.load_model("./mymodel.h5",custom_objects = {'KerasLayer':hub.kerasLayer})
-with open('label_map.json''r')as f: class_names = json.load(f)
+reload_keras_model = tf.keras.models.load_model( commands.checkpoint, custom_objects={'KerasLayer':hub.KerasLayer})
+#reload_keras_model = tf.keras.models.load_model("./mymodel.h5",custom_objects = {'KerasLayer':hub.kerasLayer})
+with open('label_map.json', 'r') as f:class_names = json.load(f)
+print(class_names)
 
 def process_image(image):
     image_size = 224
@@ -54,7 +65,10 @@ def predict(image_path, model, top_k):
     modify_np_image = np.expand_dims(modify_np_image, axis=0)
     prediction = model.predict(modify_np_image)
     probs, classes = tf.math.top_k(prediction,top_k)
-    return probs, classes
+    #probs=probs.numpy()[0]
+   # classes=classes.numpy()[0]
+    #return probs, classes
+	return probs.numpy()[0], classes.numpy()[0]
 	
-probs, classes = predict(image_path , reload_keras_model, top_k)
+probs, class_names = predict(image_path , reload_keras_model, top_k)
 
